f0a318ed2b1664215762e1fb665dfa178262608c
Jesfrin Gonzalez <j.gonzalez@kolo-app.com>
2025-05-05 20:12:58 -0600

    create basic structure

---
+++ src/main/java/com/jes/sms/SmsMessageApiApplication.java
@@ -0,0 +1,13 @@
+package com.jes.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsMessageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsMessageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jes/sms/SmsMessageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jes.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMessageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
